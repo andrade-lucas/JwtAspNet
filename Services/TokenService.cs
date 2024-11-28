27e5d379f8e5cf34abd6a7aa7c96fbe0c02a12ae
Lucas Andrade <lucasilva108@gmail.com>
2024-11-27 23:15:52 -0300

    feat: generate a valid jwt token

---
+++ Services/TokenService.cs
@@ -0,0 +1,50 @@
+﻿using JwtAspNet.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAspNet.Services;
+
+public class TokenService
+{
+    public string Create(User user)
+    {
+        var handler = new JwtSecurityTokenHandler();
+
+        var key = Encoding.UTF8.GetBytes(Configuration.PrivateKey);
+        var credentials = new SigningCredentials(
+            new SymmetricSecurityKey(key), 
+            SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            SigningCredentials = credentials,
+            Expires = DateTime.UtcNow.AddHours(2),
+            Subject = GenerateClaims(user)
+        };
+
+        new Claim("", "");
+        new Claim(ClaimTypes.Role, "");
+
+        var token = handler.CreateToken(tokenDescriptor);
+
+        return handler.WriteToken(token);
+    }
+
+    private static ClaimsIdentity GenerateClaims(User user)
+    {
+        var ci = new ClaimsIdentity();
+
+        ci.AddClaim(new Claim("id", user.Id.ToString()));
+        ci.AddClaim(new Claim(ClaimTypes.Name, user.Email));
+        ci.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+        ci.AddClaim(new Claim(ClaimTypes.GivenName, user.Name));
+        ci.AddClaim(new Claim("image", user.Image));
+
+        foreach (var role in user.Roles)
+            ci.AddClaim(new Claim(ClaimTypes.Role, role));
+
+        return ci;
+    }
+}
