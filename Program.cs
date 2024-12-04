ae286b865efcfa1467c325d896e894134bb56e7c
Lucas Andrade <lucasilva108@gmail.com>
2024-12-03 23:11:31 -0300

    feat: endpoint to create token

--- Program.cs
+++ Program.cs
@@ -27,6 +27,20 @@ var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/login", (TokenService service) =>
+{
+    var roles = new string[]
+    {
+        "user",
+        "admin"
+    };
+    var user = new User(1, "Lucas Andrade", "lucasilva108@gmail.com", "https://profile-image.com/", "12341234", roles);
+    return service.Create(user);
+});
+
+app.MapGet("/restrito", () =>
+{
+    return "Você tem acesso";
+}).RequireAuthorization();
 
 app.Run();
